7bfbc25da4ab7515d62927ff73c1b6f44af12ed1
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-18 00:18:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchies][:languages]
+		 programmer_hash[:dennis_ritchie][:languages]
 
 end
 
